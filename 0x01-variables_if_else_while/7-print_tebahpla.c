82acae2bd32d8d6cc3a27ba4c9b4bc05821fb35b
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-09 15:38:27 -0800

    Task 7 completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 112; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
