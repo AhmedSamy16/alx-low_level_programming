527e89acd3146c540b72b0d05e5fdfa1328913e6
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-09 15:51:03 -0800

    Task 9 completed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints all numbers between 0 to 9 inclusive with commas and spaces as separation
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
