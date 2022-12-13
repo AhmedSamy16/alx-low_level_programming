bd4e02dc88af4aeb04e095bdc1ff7f8b0e3a4e54
Ahmed Samy <107482535+AhmedSamy16@users.noreply.github.com>
2022-12-14 00:51:50 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,27 @@
 
 void times_table(void)
 {
-	int i, j;
+        int i, j, result;
 
-	i = 0;
-	while (i < 10)
-	{
-		j = 0;
-		while(j < 10)
-		{
-			_putchar((i * j) + 48);
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			j++;
-		}
-		_putchar('\n');
-		i++;
-	}
+        for (i = 0; i <= 9; i++)
+        {
+                _putchar(48);
+                for (j = 1; j <= 9; j++)
+                {
+                        _putchar(44);
+                        _putchar(32);
+                        result = i * j;
+                        if (result <= 9)
+                        {
+                                _putchar(32);
+                                _putchar(result + 48);
+                        }
+                        else
+                        {
+                                _putchar((result / 10) + 48);
+                                _putchar((result % 10) + 48);
+                        }
+                }
+                _putchar('\n');
+        }
 }
