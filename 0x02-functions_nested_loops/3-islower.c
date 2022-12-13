5fa6c4f288ebc1ae869b2b838c6d5a22e314c3b8
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-13 06:45:21 -0800

    Task 3 completed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Check if a letter is lowercase or not
+ * @c is a letter to be tested
+ * Return: 1 if letter is lowercase else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else 
+		return (0);
+}
