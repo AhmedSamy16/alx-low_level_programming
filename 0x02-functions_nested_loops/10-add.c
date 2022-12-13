78ba2500956a929359b2ce2fa53a934a6cf3e8d4
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-13 15:03:30 -0800

    Task 9 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - give the sum of two integers
+ * @a: first int to be summed
+ * @b: second int to be summed
+ * Return: the sum of @a and @b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
