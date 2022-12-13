f239317fde0176f2993f346a38d322357f0c75ae
Ahmed Samy <107482535+AhmedSamy16@users.noreply.github.com>
2022-12-13 16:24:32 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
+/**
+ * main - prints _putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	/**
-	 * main - prints _putchar followed by a new line
-	 * Return: Always 0 (Success)
-	 */
 	char name[] = "_putchar";
 
 	int c;
