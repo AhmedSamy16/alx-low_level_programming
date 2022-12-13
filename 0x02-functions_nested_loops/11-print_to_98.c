81cf0b29d2e0a8d43589e4b9cf95f5487424b820
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-13 15:13:31 -0800

    Task 11 completed

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all integers from n to 98
+ * @n: integer to start from
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf('\n');
+			}
+		}
+	}
+	else
+	{
+		for (j = n; j >= 98; j--)
+		{
+			printf("%d", j);
+			if (j != 98)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf('\n');
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -10,4 +10,5 @@ The Tasks For the functions and Nested projects
 8-24_hours.c => prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
 9-times_table.c => prints the 9 times table, starting with 0.
 10-add.c => return the result of adding two integers together.
+11-print_to_98 => print all integers from n to 98
 
