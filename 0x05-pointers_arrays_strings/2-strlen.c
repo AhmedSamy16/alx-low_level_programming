70ca9cc3b00ad65b289b7373ce4086ecc6f1c540
Ahmed Samy <107482535+AhmedSamy16@users.noreply.github.com>
2022-12-19 23:33:29 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,13 @@
 /**
  * _strlen - Finds the length of a string
  * @s: the string to find its length
- * return: int of the length of the string
+ * Return: int of the length of the string
  */
 
 int _strlen(char *s)
 {
-	int i, len;
+	int i;
+	int len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		len++;
