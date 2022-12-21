06aaf5356bd38e481341f726ee93fc78d8eb3f7c
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-21 12:41:49 -0800

    Task 2 completed

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the string pointed by source to the buffer pointed by dest.
+ * @dest: the destination
+ * @src: the string to be copied
+ * @n: number
+ * Return: return a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c => Concatenate Two strings .
 1-strncat.c => concatenate two strings.
+2-strncpy.c => copies a string to a specific buffer.
 
