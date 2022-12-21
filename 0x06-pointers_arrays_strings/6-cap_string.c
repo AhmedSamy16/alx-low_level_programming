5c915260b28dbf8b71ae19d18a620734e36668f9
Ahmed Samy <ahmed.samy1640@gmail.com>
2022-12-21 13:21:23 -0800

    Task 6 completed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @s: a string
+ * Return: a string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep_word[] = {' ', '\n', '\t', ',', ';', '!', '?', '"', '.', '(', ')', '{'. '}'};
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] -= 32;
+			continue;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i - 1] == sep_word[j] && s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] -= 32;
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,4 +3,5 @@
 2-strncpy.c => copies a string to a specific buffer.
 3-strcmp.c => compares two strings and return 0 if they are equal and another number otherwise4-rev_array.c = > reverse an array on integers.
 5-string_toupper.c => turns a lowercase string to an uppercase.
+6-cap_string.c => capitalize all words of a string.
 
