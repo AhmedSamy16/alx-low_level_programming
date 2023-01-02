7f221e310e068de859dc88ed4a2e30e5055e8f02
Ahmed Samy <ahmed.samy1640@gmail.com>
2023-01-02 15:15:09 -0800

    Task 4 completed

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: the string to be searched
+ * @accept: the string to be found
+ * Return: *s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 1-memcpy.c => copies memory area.
 2-strchr.c => locates a character in a string.
 3-strspn.c =>  gets the length of a prefix substring.
+4-strpbrk.c => searches a string for any of a set of bytes.
 
