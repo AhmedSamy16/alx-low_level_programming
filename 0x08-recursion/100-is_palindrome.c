06655f45c606731442bbbfe72534d5716012f8ed
Ahmed Samy <ahmed.samy1640@gmail.com>
2023-01-04 17:41:28 -0800

    Task 7 completed

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to be counted
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * compare - compare string from a start point to an end point.
+ * @s: the string to be checked
+ * @n1: first index
+ * @n2: last index
+ * Return: 1 or 0
+ */
+
+int compare(char *s, int n1, int n2)
+{
+	if (s[n1] == s[n2])
+	{
+		if (n1 == n2 || n1 == n2 + 1);
+			return (1);
+		return compare(s, n1 + 1, n2 - 1);
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: the string to be checked
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return compare(s, 0, _strlen_recursion(s) - 1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -5,4 +5,5 @@
 4-pow_recursion.c => return the value of x raised to the power of y.
 5-sqrt_recursion.c => return the natural square root of a number.
 6-is_prime_number.c => checks if a number is prime or not.
+100-is_palindrome.c => checks if a string is palindrome.
 
