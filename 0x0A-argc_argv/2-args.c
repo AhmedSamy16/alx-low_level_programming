d49461f79ef6d26b3d506dc70f00548cadc4c2d9
Ahmed Samy <ahmed.samy1640@gmail.com>
2023-01-06 15:19:31 -0800

    Task 2 completed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all the arguments passed to the program.
+ * @argc: the number of arguments passed to the program.
+ * @argv: An array of pointers to the argumnets.
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 0-whatsmyname.c => Prints the name of the program's file.
 1-args.c => Prints the number of arguments passed to the program.
+2-args.c => Prints the arguments passed to the program.
 
